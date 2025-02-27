f4fea7290b3d73c669cda3468f70ad0d9100a974
Aku52 <kotik.52@tutamail.com>
2025-02-27 17:08:41 +0300

    пример

---
+++ class.py
@@ -0,0 +1,27 @@
+class Birds:
+    def __init__(self,name,flying_ability,size):
+        self.name = name
+        self.flying_ability = flying_ability
+        self.size = size
+        
+    def soinding (self):
+        print(f"птичка {self.name} поёт утром")
+
+    def eating (self):
+        print(f"птичка {self.name} ест днём")
+
+    def sleeping (self):
+        print(f"птичка {self.name} спит ночью")
+
+    def flying_range(self,N):
+        if self.flying_ability ==True:
+            print( self.size * N)
+
+        else:
+            print ("error ")
+ 
+    def __del__(self):
+        print("del done")
+
+vorobey  = Birds ("vorobey",True,50 )      
+vorobey.flying_range (10)
