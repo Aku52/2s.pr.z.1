f5edb6dfc178894fa69859bb18e5be4ff8aa223a
Aku52 <kotik.52@tutamail.com>
2025-09-08 15:48:18 +0300

    great

--- mood.py
+++ mood.py
@@ -6,19 +6,22 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
 text = [
-    "купи ффон за 100 зубрей",#spam
-    "стрелка в офисе в 10:ОО",#not spam
-    "вы выграли милллллион, пришлити CVV",#spam
-    "отчет продажи за квартал 2025",#not spam
-    "срочно позвони сейчас и получи приз",#spam
-    "напоминаем про оплату счета"#not spam
+    "Ж",#not dead
+    "М",#not dead
+    "B",#not dead
+    "С",#dead
+    "A",#not dead
+    "10",# dead
+    "20",#not dead
+    "50",#not dead
+    "70",#dead
+    "90",#dead
 ]
 
-lable = [1,0,1,0,1,0] #1 - spam, 0 - not spam
+lable = [0,0,0,1,0,1,0,0,1,1] #1 - dead, 0 - not dead
 
 text_train, text_test, y_train, y_test = train_test_split(text, lable, test_size = 0.33, random_state=42)
-print(text_test)
-print(y_test)
+
 pipe = make_pipeline(
     CountVectorizer(),# задача сделть мешок слов, это подсчет колличества вхождения слов игнорируя их порядковый номер, на выходе получается матрица
     MultinomialNB() #учит верооятности слов 
@@ -27,8 +30,15 @@ pipe = make_pipeline(
 pipe.fit(text_train, y_train)
 y_pred = pipe.predict(text_test)
 
-print(y_pred)
-print(f"Accuracy:{accuracy_score(y_test,y_pred)}")
-print("New:", pipe.predict(["поздравляем вы победитель пришлите нам свой CVV код"])[0])
-print("New:", pipe.predict(["Дообрый вечер встреча завтра в 12:00"])[0])
 
+print(f"Accuracy:{accuracy_score(y_test,y_pred)}")
+
+if (pipe.predict(["Ж 90 С"])[0]) == 0:
+    print("this person not dead")
+else:
+    print("this person dead")
+
+if (pipe.predict(["М 10 C"])[0]) == 0:
+    print("this person not dead")
+else:
+    print("this person dead")
---
+++ work.py
@@ -0,0 +1,46 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
+
+price = np.array([1, 1.1, 1.4, 1.7, 2, 2.5, 3, 3.1, 2.3, 2.1, 1.4, 1])
+temp = np.array([-5, -2, 5, 7, 15, 23, 30, 32, 26, 16, 10, -5])
+
+print("ВВЕДИТЕ НОМЕР МЕСЯЦА:")
+data = input()
+
+
+def moving_average(series, data):
+    
+    if len(series) < int(data):
+           return np.mean(series)
+    return np.mean(series[-(int(data))])
+
+def moving_average_month(series, data):
+    
+    if len(series) < int(data):
+           return np.mean(series)
+    return np.mean(series[-(int(data))])
+
+ma_pred = moving_average(price, int(data))
+ma_pred = moving_average_month(temp, int(data))
+
+
+x = np.arange(len(price)).reshape(-1,1)#weeks(0,1,2,3)
+y = price
+
+x_1= np.arange(len(temp)).reshape(-1,1)#weeks(0,1,2,3)
+y_1 = temp
+
+model = LinearRegression()
+model.fit(x,y)
+
+model_month = LinearRegression()
+model_month.fit(x_1,y_1)
+
+next_price = np.array([[len(price)]])
+linear_pred = model.predict(next_price)[0]
+
+next_month = np.array([[len(temp)]])
+linear_pred_month = model_month.predict(next_month)[0]
+
+print(f"with temp:{linear_pred_month} price for icecream in {data}: {linear_pred}")
